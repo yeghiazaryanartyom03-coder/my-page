e707428fbf6d61e8014de89f7e618ddceaef5807
bomj22bomj <bomjovicbomj@gmail.com>
2026-03-15 15:31:45 +0400

    spreading seeds

---
+++ backend/src/seed/seedArrays/itemSeed.ts
@@ -0,0 +1,323 @@
+export const itemSeed = [
+  {
+    id: "1",
+    title: "Goalkeeper socks 25/26",
+    image: "/pictures/items/goalkeeper-socks.webp",
+    price: 1999,
+    review: {
+      stars: 8.6,
+      reviews: 1243,
+    },
+  },
+  {
+    id: "2",
+    title: "Goalkeeper shorts 25/26",
+    image: "/pictures/items/goalkeeper-shorts.webp",
+    price: 4999,
+    review: {
+      stars: 8.6,
+      reviews: 243,
+    },
+  },
+  {
+    id: "3",
+    title: "Goalkeeper jersey 25/26",
+    image: "/pictures/items/goalkeeper-jersey.webp",
+    price: 12499,
+    review: {
+      stars: 8.8,
+      reviews: 3600,
+    },
+  },
+  {
+    id: "4",
+    title: "third socks 25/26",
+    image: "/pictures/items/third-socks.webp",
+    price: 1999,
+    review: {
+      stars: 10,
+      reviews: 976,
+    },
+  },
+  {
+    id: "5",
+    title: "third shorts 25/26",
+    image: "/pictures/items/third-shorts.webp",
+    price: 7499,
+    review: {
+      stars: 10,
+      reviews: 500,
+    },
+  },
+  {
+    id: "6",
+    title: "third jersey 25/26",
+    image: "/pictures/items/third-jersey.webp",
+    price: 9999,
+    review: {
+      stars: 9.9,
+      reviews: 1232,
+    },
+  },
+  {
+    id: "7",
+    title: "Baby's third kit 25/26",
+    image: "/pictures/items/baby-third-form.webp",
+    price: 6499,
+    review: {
+      stars: 8.9,
+      reviews: 122,
+    },
+  },
+  {
+    id: "8",
+    title: "fourth socks 25/26",
+    image: "/pictures/items/fourth-socks.webp",
+    price: 1999,
+    review: {
+      stars: 7.9,
+      reviews: 232,
+    },
+  },
+  {
+    id: "9",
+    title: "fourth shorts 25/26",
+    image: "/pictures/items/fourth-shorts.webp",
+    price: 7499,
+    review: {
+      stars: 7.8,
+      reviews: 800,
+    },
+  },
+  {
+    id: "10",
+    title: "fourth jersey 25/26",
+    image: "/pictures/items/fourth-jersey.webp",
+    price: 9999,
+    review: {
+      stars: 8.8,
+      reviews: 550,
+    },
+  },
+  {
+    id: "11",
+    title: "Baby's fourth kit 25/26",
+    image: "/pictures/items/baby-fourth-kit.webp",
+    price: 6499,
+    review: {
+      stars: 8.2,
+      reviews: 654,
+    },
+  },
+  {
+    id: "12",
+    title: "home socks 25/26",
+    image: "/pictures/items/home-socks.webp",
+    price: 1999,
+    review: {
+      stars: 8.4,
+      reviews: 939,
+    },
+  },
+  {
+    id: "13",
+    title: "home shorts 25/26",
+    image: "/pictures/items/home-shorts.webp",
+    price: 7499,
+    review: {
+      stars: 8.5,
+      reviews: 839,
+    },
+  },
+  {
+    id: "14",
+    title: "home jersey 25/26",
+    image: "/pictures/items/ucl-jersey.webp",
+    price: 9999,
+    review: {
+      stars: 8.4,
+      reviews: 935,
+    },
+  },
+  {
+    id: "15",
+    title: "Baby's home kit 25/26",
+    image: "/pictures/items/baby-home-kit.webp",
+    price: 6999,
+    review: {
+      stars: 8.1,
+      reviews: 1211,
+    },
+  },
+  {
+    id: "16",
+    title: "away socks golakeeper 25/26",
+    image: "/pictures/items/away-socks-goalkeeper.webp",
+    price: 1999,
+    review: {
+      stars: 8.1,
+      reviews: 439,
+    },
+  },
+  {
+    id: "17",
+    title: "away shorts goalkeeper 25/26",
+    image: "/pictures/items/away-shorts-goalkeeper.webp",
+    price: 4999,
+    review: {
+      stars: 8.5,
+      reviews: 739,
+    },
+  },
+  {
+    id: "18",
+    title: "away jersey goalkeeper 25/26",
+    image: "/pictures/items/away-jersey-goalkeeper.webp",
+    price: 9999,
+    review: {
+      stars: 8.7,
+      reviews: 1939,
+    },
+  },
+  {
+    id: "18",
+    title: "away socks 25/26",
+    image: "/pictures/items/away-socks.webp",
+    price: 1999,
+    review: {
+      stars: 8.4,
+      reviews: 99,
+    },
+  },
+
+  {
+    id: "19",
+    title: "away shorts 25/26",
+    image: "/pictures/items/away-shorts.webp",
+    price: 7499,
+    review: {
+      stars: 8.1,
+      reviews: 199,
+    },
+  },
+  {
+    id: "20",
+    title: "away jersey 25/26",
+    image: "/pictures/items/away-jersey.webp",
+    price: 9999,
+    review: {
+      stars: 8.4,
+      reviews: 4567,
+    },
+  },
+  {
+    id: "21",
+    title: "Retro Jersey FC Barcelona",
+    image: "/pictures/items/retro-jersey.webp",
+    price: 5999,
+    review: {
+      stars: 9.1,
+      reviews: 1939,
+    },
+  },
+  {
+    id: "22",
+    title: "MESSI | Lionel Messi 20/21 Signed Home Shirt",
+    image: "/pictures/items/leo-messi-jersey.webp",
+    price: 279999,
+    review: {
+      stars: 8.4,
+      reviews: 939,
+    },
+  },
+  {
+    id: "23",
+    title: "Bucket",
+    image: "/pictures/items/bucket.webp",
+    price: 3499,
+    review: {
+      stars: 9.2,
+      reviews: 845,
+    },
+  },
+  {
+    id: "24",
+    title: "Backpack Navy",
+    image: "/pictures/items/backpack.webp",
+    price: 7999,
+    review: {
+      stars: 8.5,
+      reviews: 1234,
+    },
+  },
+  {
+    id: "25",
+    title: "Crew Navy Fan Club",
+    image: "/pictures/items/crew-navy.webp",
+    price: 5999,
+    review: {
+      stars: 8.8,
+      reviews: 1534,
+    },
+  },
+  {
+    id: "26",
+    title: "Jersey Retro Basket 94",
+    image: "/pictures/items/basketball.webp",
+    price: 6999,
+    review: {
+      stars: 9.5,
+      reviews: 1167,
+    },
+  },
+  {
+    id: "27",
+    title: "Retro Jersey 90-92",
+    image: "/pictures/items/retro-jersey-90.webp",
+    price: 6999,
+    review: {
+      stars: 8.1,
+      reviews: 637,
+    },
+  },
+  {
+    id: "28",
+    title: "T-shirt Fan Club Barca",
+    image: "/pictures/items/t-shirt.webp",
+    price: 2999,
+    review: {
+      stars: 9.2,
+      reviews: 2234,
+    },
+  },
+  {
+    id: "29",
+    title: "Jacket Barca",
+    image: "/pictures/items/jacket.webp",
+    price: 13499,
+    review: {
+      stars: 8.1,
+      reviews: 745,
+    },
+  },
+  {
+    id: "30",
+    title: "pant tech Barca",
+    image: "/pictures/items/pant.webp",
+    price: 11499,
+    review: {
+      stars: 8.2,
+      reviews: 534,
+    },
+  },
+  {
+    id: "31",
+    title: "Vintage 80/81 Jersey",
+    image: "/pictures/items/vintage-jersey.webp",
+    price: 6999,
+    review: {
+      stars: 9.9,
+      reviews: 5234,
+    },
+  },
+];
---
+++ backend/src/seed/seedArrays/newsSeed.ts
@@ -0,0 +1,50 @@
+export const newsSeed = [
+  {
+    title: "Former Barcelona star Rafinha Alcantara announces retirement",
+    author: "by Bomj",
+    views: 190,
+    image: "/pictures/news-images/raphinha-img.webp",
+  },
+  {
+    title: "Gerard Martín makes his mark in 2025",
+    author: "by Bomj",
+    views: 10000,
+    image: "/pictures/news-images/Gerard.webp",
+  },
+  {
+    title: "Lamine Yamal is the king of dribbling",
+    author: "by Bomj",
+    views: 90,
+    image: "/pictures/news-images/Yamal.webp",
+  },
+  {
+    title: "Comeback against Atlético, the best moment from December",
+    author: "by Bomj",
+    views: 321,
+    image: "/pictures/news-images/team.webp",
+  },
+  {
+    title: "Barca equal an 82-year-old record",
+    author: "by Bomj",
+    views: 190,
+    image: "/pictures/news-images/record.webp",
+  },
+  {
+    title: "2025 in five stunning stats",
+    author: "by Bomj",
+    views: 190,
+    image: "/pictures/news-images/five_stunning.webp",
+  },
+  {
+    title: "What's happened when Barça were winter champions?",
+    author: "by Bomj",
+    views: 190,
+    image: "/pictures/news-images/hug.webp",
+  },
+  {
+    title: "Hansi Flick's squad to return to training on 29 December",
+    author: "by Bomj",
+    views: 190,
+    image: "/pictures/news-images/trainig.webp",
+  },
+];
